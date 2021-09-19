7c8b456a2d4ab34d94fac631b11d9688cd95f420
ErikErikHg <90993908+ErikErikHg@users.noreply.github.com>
2021-09-19 20:59:56 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,181 @@
+//HAI KALI INI SAYA MAU MEMBERIKAN SC, MOHON TAMBAH NAMA SAYA PRII
+//PASTI BANYAK YANG LANGGAR
+const help = (prefix) => {
+	return `Hallo, user bot 👋
+Semoga harimu Menyenangkan 
+
+     LIST MENU  ${namabot}
+       
+ ╭◪ INFO CREATOR
+├❖ *Author : BOT INDO*
+├❍ *Insta    : @aprigans_*
+├❖ *Youtub : BOT INDO*
+├❍ *LinkGb : https://chat.whatsapp.com/Hk1IyJnQndE1sXtc1eYbFB*
+├❖ *LINK : https://www.youtube.com/c/BOTINDO*
+├❍ *wasap : wa.me/628811929177*
+╰────────────────────────╯
+       
+╭─❒ *info bot* 
+│❒ 𝖭𝖺𝗆𝖺 : ${namabot}
+│❒ author : Prii
+│❒ recode : Bot Indo
+│❒ server : baileys
+╰───────────────┈ ⳹
+
+╭─❒ *INFO OWNER* 
+│❒ 𝖭𝖺𝗆𝖺 : ${namaowner}
+│❒ Nomor : wa.me/${owner}
+╰───────────────┈ ⳹
+
+╭◪ 𝐑𝐮𝐥𝐞𝐬𝐒 
+╰────────────────────────╮  
+╭────────────────────────╯
+├❖ *Spam : Auto Block!*
+├❖ *Beri Jeda 5 Detik!*
+├❖ *No Vc/Tlpn=Block!*
+├❖ *Donasi Sultan Buat Apikey!*
+├❖ *Note Bot Belum Jadi Sepenuhnya!*
+╰────────────────────────╯
+
+╭◪ Simpel
+├⊱❥ *${prefix}info*
+├⊱❥ *${prefix}donasi*
+├⊱❥ *${prefix}owner*
+├⊱❥ *${prefix}creator*
+├⊱❥ *${prefix}sc*
+├⊱❥ *${prefix}request*
+├⊱❥ *${prefix}rules*
+├⊱❥ *${prefix}iklan*
+├⊱❥ *${prefix}bingungcok*
+╰───────────────────────╯
+╭◪ Maker Menu
+├❖ *${prefix}tahta [text]*
+├❖ *${prefix}wetglass [text]*
+├❖ *${prefix}pornhub [text&text]*
+├❖ *${prefix}glitch [text&text]*
+├❖ *${prefix}multicolor3d [text]*
+├❖ *${prefix}neon [text]*
+├❖ *${prefix}pubg [text]*
+├❖ *${prefix}blackpink [text]*
+├❖ *${prefix}lovemake [text]*
+├❖ *${prefix}logoepep [text]*
+├❖ *${prefix}logoepep2 [text]*
+├❖ *${prefix}logoepep3 [text]*
+├❖ *${prefix}logoepep4 [text]*
+├❖ *${prefix}logoepep5 [text]*
+├❖ *${prefix}lighttext [text]*
+╰───────────────────────╯
+╭◪ Stiker Menu
+├❍ *${prefix}ttp [text]*
+├❍ *${prefix}attp [text]*
+├❍ *${prefix}attp2 [text]*
+├❍ *${prefix}attp3 [text]*
+├❍ *${prefix}attp4 [text]*
+├❍ *${prefix}temoji 🗿*
+├❍ *${prefix}stiker [foto/tag]*
+├❍ *${prefix}sticker [video/foto]*
+├❍ *${prefix}toimg [tag stiker]*
+╰───────────────────────╯
+╭◪ Edukasi
+├⊱❥ *${prefix}brainly*
+├⊱❥ *${prefix}nulis*
+├⊱❥ *${prefix}nulisbkiri*
+├⊱❥ *${prefix}nulisbkanan*
+├⊱❥ *${prefix}nulispolio*
+╰───────────────────────╯
+╭◪ Umum
+├❖ *${prefix}pinterest [cari gambar]*
+├❖ *${prefix}quotes*
+├❖ *${prefix}bucin*
+├❖ *${prefix}tts [kode bahsa]*
+╰───────────────────────╯
+╭◪ Group 
+│➸ *${prefix}welcome* ⟪ 1/0 ⟫
+│➸ *${prefix}antilink* ⟪ 1/0 ⟫
+│➸ *${prefix}leveling* ⟪ 1/0 ⟫
+│➸ *${prefix}event* ⟪ 1/0 ⟫
+│➸ *${prefix}simih* ⟪ 1/0 ⟫
+│➸ *${prefix}grup* ⟪ buka/tutup ⟫
+│➸ *${prefix}add*
+├❖ *${prefix}getpp @tag /group *
+│➸ *${prefix}kick*
+│➸ *${prefix}hedsot*
+│➸ *${prefix}linkgrup*
+│➸ *${prefix}promote*
+│➸ *${prefix}demote*
+│➸ *${prefix}setname*
+│➸ *${prefix}setdesc*
+│➸ *${prefix}leave*
+│➸ *${prefix}tagall*
+│➸ *${prefix}listadmin*
+│➸ *${prefix}level*
+│➸ *${prefix}fitnah*
+│➸ *${prefix}hidetag*
+│➸ *${prefix}hidetag5*
+╰───────────────────────╯
+╭◪ Anime 
+├⊱❥ *${prefix}anime*
+├⊱❥ *${prefix}animesaran2*
+├⊱❥ *${prefix}animesaran*
+├⊱❥ *${prefix}neko*
+├⊱❥ *${prefix}nekonime*
+├⊱❥ *${prefix}wibu*
+├⊱❥ *${prefix}loli*
+├⊱❥ *${prefix}waifu*
+├⊱❥ *${prefix}husbu*
+├⊱❥ *${prefix}pokemon*
+├⊱❥ *${prefix}anjing*
+├⊱❥ *${prefix}naruto*
+├⊱❥ *${prefix}sagiri*
+├⊱❥ *${prefix}minato*
+├⊱❥ *${prefix}sakura*
+├⊱❥ *${prefix}itori*
+├⊱❥ *${prefix}hinata*
+├⊱❥ *${prefix}kaneki*
+├⊱❥ *${prefix}miku*
+├⊱❥ *${prefix}unta*
+├⊱❥ *${prefix}rize*
+├⊱❥ *${prefix}akira*
+├⊱❥ *${prefix}kurumi*
+├⊱❥ *${prefix}pictcewek*
+├⊱❥ *${prefix}pictcowok*
+├⊱❥ *${prefix}ram*
+├⊱❥ *${prefix}rem*
+├⊱❥ *${prefix}toukacahan*
+╰──────────────────────╯
+╭◪ Download
+├❖ *${prefix}play [judul no link]*
+├❖ *${prefix}ytmp3 [perbaikan]*
+├❖ *${prefix}ytmp4 [perbaikan]*
+├❖ *${prefix}yutubdl [link]*
+├❖ *${prefix}joox*
+╰───────────────────────╯
+╭◪ Owner Bot
+├❖ *${prefix}bc*
+├❖ *${prefix}clearall*
+├❖ *${prefix}block*
+├❖ *${prefix}unblock*
+├❖ *${prefix}clone*
+├❖ *${prefix}setppbot*
+├❖ *${prefix}setreply*
+├❖ *${prefix}ban*
+├❖ *${prefix}unban*
+├❖ *${prefix}bcimg*
+╰────────────────────────╯
+
+_*BIG THANKS TO*_
+*ALLAH SWT*
+*MHANKBARBAR*
+*DINATA (MY GURU)*
+*PRII*
+*ᴵᵀˢ Me Xinzz*
+*REZZ (SI BAU)*
+*BOT INDO*
+
+           ║▌│█║▌│ █║▌│█│║▌║
+           ║▌│█║▌│ █║▌│█│║▌║
+   
+            [ *POWERED BY ${namaowner}* ]`
+}
+
+exports.help = help
